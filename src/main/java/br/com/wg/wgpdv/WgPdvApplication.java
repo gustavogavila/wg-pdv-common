de1e3bf31620e4e37a60f8d467db3cc49d7bae93
Gustavo Avila <gustavogavila01@gmail.com>
2018-10-18 15:09:39 -0300

    Primeiro commit

---
+++ src/main/java/br/com/wg/wgpdv/WgPdvApplication.java
@@ -0,0 +1,12 @@
+package br.com.wg.wgpdv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WgPdvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WgPdvApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/wg/wgpdv/WgPdvApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.wg.wgpdv;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WgPdvApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
